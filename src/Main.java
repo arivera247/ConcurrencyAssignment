255c082a33a7fb8109459aad5ca65877d3492d62
Alex <9136079+arivera247@users.noreply.github.com>
2023-04-03 19:43:15 -0400

    Added single thread sum and calculation.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(RNG.createRandomNumber());
+        LocalTime startTimeSingleThread = LocalTime.now();
+        for (int i = 0; i < 200000000; i++) {
+            RNG.createRandomNumber();
+        }
+        LocalTime endTimeSingleThread = LocalTime.now();
+        System.out.println("Single thread time: " + Math.abs(endTimeSingleThread.getNano() - startTimeSingleThread.getNano()));
     }
 }
+
+
+
+//Current PC has 8 cores and 16 threads
